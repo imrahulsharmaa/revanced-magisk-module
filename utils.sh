ffcf535ba6712a2a84271be256c2013c8a3dfdbe
Rahul Vishwakarma <94642476+imrahulsharmaa@users.noreply.github.com>
2026-07-21 08:35:43 +0530

    Refactor patch_apk function command construction
    Updated command construction in patch_apk function to include additional options for handling CLI jar and Android OS. Added a TODO comment for future removal.

--- utils.sh
+++ utils.sh
@@ -336,11 +336,19 @@ merge_splits() {
 	# sign the merged stock apk
 	patch_apk() {
 	local stock_input=$1 patched_apk=$2 patcher_args=$3 cli_jar=$4 patches_jar=$5
-	local cmd="java -jar '$cli_jar' patch '$stock_input' -o '$patched_apk' -p '$patches_jar' --keystore=ks.keystore \
---keystore-entry-password=123456789 --keystore-password=123456789 --signer=jhc --keystore-entry-alias=jhc -t '$patched_apk-tmp' $patcher_args"
+	local cmd="java -jar '$cli_jar' patch '$stock_input' -o '$patched_apk' -p '$patches_jar' --keystore=ks.keystore --keystore-entry-password=123456789 --keystore-password=123456789 --signer=jhc --keystore-entry-alias=jhc -t '$patched_apk-tmp' $patcher_args"
+
+	# TODO: remove this later
+	local cli_name
+	cli_name=$(basename "$cli_jar")
+	if [ "${cli_name::8}" = revanced ]; then cmd+=" -b"; fi
+
+	if [ "$OS" = Android ]; then cmd+=" --custom-aapt2-binary='${AAPT2}'"; fi
+	pr "$cmd"
+	if eval "$cmd"; then [ -f "$patched_apk" ]; else
+		rm "$patched_apk" 2>/dev/null || :
+		return 1
 	fi
-	rm "${output}.idsig" "${output}-unsigned" 2>/dev/null || :
-	return 0
 }
 
 # -------------------- apkmirror --------------------
